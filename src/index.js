1173bd26183a8471ad13a174334a5220fa8ea86e
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-11-12 00:50:07 +0100

    :construction: Use static files

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import opn from 'opn';
 const app = express();
 app.use(express.static(`${__dirname}/public/`));
 
-app.get('/', (req, res) => { //get ajax request and response
-  res.sendfile(`${__dirname}/public/index.html`); //send to browser
-  console.log(res);
-
-});
-
 app.listen(3000); //listen to port 3000
 
 opn('http://localhost:3000'); //open browser
